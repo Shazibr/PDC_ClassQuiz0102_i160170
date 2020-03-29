2efb33073b53db799763d5604b5d82b4f22bade8
Shazibr <shazibrauf@hotmail.com>
2020-03-29 14:01:52 +0500

    first commit

---
+++ PDC_ClassQuiz0102_i160170.go
@@ -0,0 +1,26 @@
+package PDC_ClassQuiz0102_i160170
+
+import (
+	"fmt"
+)
+
+
+func PakistanCases(){
+	fmt.Println("The total number of Corona Virus cases in Pakistan are: 1408")
+}
+
+func SouthKoreaCases(){
+	fmt.Println("The total number of Corona Virus cases in South Korea are: 32,964")
+}
+
+func FranceCases(){
+	fmt.Println("The total number of Corona Virus cases in France are: 9,478")
+}
+
+func PersonalizedMessage(){
+	fmt.Println("My personalized message is:")
+	fmt.Println(" -> Wash your hands with soap and water or use sanitizer.")
+	fmt.Println(" -> Maintain at least 1 metre (3 feet) distance.")
+	fmt.Println(" -> Avoid touching eyes, nose and mouth.")
+	fmt.Println(" -> Stay at home.")
+}
